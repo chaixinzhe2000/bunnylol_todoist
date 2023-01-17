f4379b27395762a34d6c569fa9ca492f89854618
Xinzhe Chai <xinzhe_chai@brown.edu>
2023-01-17 16:21:15 -0700

    auto capitalizing

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import * as chrono from 'chrono-node';
 export const app = express();
 const api = new TodoistApi(process.env.TODO_API_TOKEN)
 
+const capitalize = (s) => {
+	return s && s[0].toUpperCase() + s.slice(1);
+}
+
 app.get("/", (req, res) => {
 
 	res.send("Express on Vercel");
@@ -19,10 +23,10 @@ app.get("/add", async (req, res) => {
 			const timeString = todoString.substring(todoString.indexOf("@") + 1)
 			const dueDate = chrono.parseDate(timeString, { timezone: "EST" })
 			assignmentString = todoString.substring(0, todoString.indexOf("@"))
-			addResponse = await api.addTask({ content: assignmentString, dueDatetime: dueDate })
+			addResponse = await api.addTask({ content: capitalize(assignmentString), dueDatetime: dueDate })
 		} else {
 			assignmentString = todoString
-			addResponse = await api.addTask({ content: assignmentString })
+			addResponse = await api.addTask({ content: capitalize(assignmentString) })
 		}
 		res.redirect(addResponse.url);
 	} catch {
