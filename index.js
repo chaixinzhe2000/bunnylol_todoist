a67ba7244d0e309499ef12cf5659f3f3e03eef58
Xinzhe Chai <xinzhe_chai@brown.edu>
2023-01-17 15:39:21 -0700

    added parsing time

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 import express from "express"
 import { TodoistApi } from "@doist/todoist-api-typescript"
+import * as chrono from 'chrono-node';
 
 export const app = express();
 const api = new TodoistApi(process.env.TODO_API_TOKEN)
 
 app.get("/", (req, res) => {
+
 	res.send("Express on Vercel");
 });
 
 app.get("/add", async (req, res) => {
+	const todoString = req.query.todo;
+	let assignmentString, addResponse
+
 	try {
-		const addResponse = await api.addTask({ content: req.query.todo })
+		if (todoString.includes("@")) {
+			const timeString = todoString.substring(todoString.indexOf("@") + 1)
+			const dueDate = chrono.parseDate(timeString)
+			assignmentString = todoString.substring(0, todoString.indexOf("@"))
+			addResponse = await api.addTask({ content: assignmentString, dueDatetime: dueDate })
+		} else {
+			assignmentString = todoString
+			addResponse = await api.addTask({ content: assignmentString })
+		}
 		res.redirect(addResponse.url);
 	} catch {
 		res.send("Failed to add the todo item, please try again")
